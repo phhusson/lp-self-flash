d3890cae94f52bdd8afb5390ade1b52868f53cea
Pierre-Hugues Husson <phh@phh.me>
2020-05-09 14:36:04 +0200

    Finish with a reboot, we dont want to inadvertenly change partition table again

--- flash.sh
+++ flash.sh
@@ -13,3 +13,4 @@ adb shell 'su -c "/data/local/tmp/lptools unmap system_phh"'
 dmDevice=$(adb shell 'su -c "/data/local/tmp/lptools map system_phh"'|grep -oE '/dev/block/[^ ]*')
 pv $systemImg | adb shell -T -e none 'su -c "/data/local/tmp/simg2img_simple > '$dmDevice'"'
 adb shell 'su -c "/data/local/tmp/lptools replace system_phh system"'
+adb reboot
